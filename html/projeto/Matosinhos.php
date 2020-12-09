72b70aac3ea72e694f1e825697be5d5f5c25e73b
YsabellaRomero <75682047+YsabellaRomero@users.noreply.github.com>
2020-12-09 00:21:08 +0000

    second commit

--- html/projeto/Matosinhos.php
+++ html/projeto/Matosinhos.php
@@ -8,6 +8,23 @@
     <?php include("menu.php"); ?>
 
     <div class="instalacoes_info">
-      <h2><a href="instalacoes.php">Instalações</a> &gt; Matosinhos</h2>
+      <h2>
+        <ul>
+          <li class="dropdown">
+            <a href="instalacoes.php" class="dropbtn">Instalações &gt; </a>
+            <div class="dropdown-content">
+              <a href="Boavista.php">Boavista</a>
+              <a href="Matosinhos.php">Matosinhos</a>
+              <a href="Braga.php">Braga</a>
+            </div>
+          </li>
+          <li><a href="#">Matosinhos</a></li>
+        </ul>
+      </h2>
+      <ul>
+        <li><a href="contactos_boavista.php">Contactos</a></li>
+        <li><a href="horarios_boavista.php">Horarios</a></li>
+      </ul>
+    </div>
   </body>
 </html>
