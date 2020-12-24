56d53ab97750342a933a57c7b80adf8d06df031a
mariasanches <up201708991@fe.up.pt>
2020-12-24 00:05:39 +0000

    funções log e reg

---
+++ html/projeto/database/log.php
@@ -0,0 +1,10 @@
+<?php
+
+function loginValido($email,$password){
+  global $dbh;
+  $stmt = $dbh->prepare('SELECT name FROM Pessoa JOIN Socio USING (NIF) WHERE email = (?) AND password = (?)');
+  $stmt->execute(array($email, sha1($password)));
+  return $stmt->fetch();          //devolve a coluna ou false
+}
+
+ ?>
