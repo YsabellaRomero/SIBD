3e1f910c1eb7e1d7853045b98f16c7655ed547c8
mariasanches <up201708991@fe.up.pt>
2020-12-26 16:41:55 +0000

    titulo search corrigido

--- html/projeto/database/search.php
+++ html/projeto/database/search.php
@@ -17,14 +17,14 @@
 
   function searchmodalidades($search){
     global $dbh;
-    $stmt = $dbh->prepare('SELECT Modalidade.nome FROM Pessoa JOIN Treinador USING (NIF) JOIN Treino USING (id_gym) JOIN Instalacao ON Instalacao.id=Treino.localizacao JOIN Aula ON ref_treino=Treino.ID JOIN Modalidade ON id_modalidade=Modalidade.ID WHERE Pessoa.name LIKE ? AND Instalacao.localizacao=? GROUP BY Modalidade.nome');
+    $stmt = $dbh->prepare('SELECT Modalidade.nome , Pessoa.name FROM Pessoa JOIN Treinador USING (NIF) JOIN Treino USING (id_gym) JOIN Instalacao ON Instalacao.id=Treino.localizacao JOIN Aula ON ref_treino=Treino.ID JOIN Modalidade ON id_modalidade=Modalidade.ID WHERE Pessoa.name LIKE ? AND Instalacao.localizacao=? GROUP BY Modalidade.nome');
     $stmt->execute(array("%$search%", $_SESSION['local']));
     return $stmt->fetchall();
   }
 
   function searchtreinador($search) {
     global $dbh;
-    $stmt = $dbh->prepare('SELECT Pessoa.name FROM Pessoa JOIN Treinador USING (NIF) JOIN Treino USING (id_gym) JOIN Instalacao ON Instalacao.id=Treino.localizacao JOIN Aula On ref_treino=Treino.ID Join Modalidade ON id_modalidade=modalidade.ID WHERE Modalidade.nome LIKE ? AND Instalacao.localizacao=? GROUP BY Pessoa.name');
+    $stmt = $dbh->prepare('SELECT Pessoa.name , Modalidade.nome FROM Pessoa JOIN Treinador USING (NIF) JOIN Treino USING (id_gym) JOIN Instalacao ON Instalacao.id=Treino.localizacao JOIN Aula On ref_treino=Treino.ID Join Modalidade ON id_modalidade=modalidade.ID WHERE Modalidade.nome LIKE ? AND Instalacao.localizacao=? GROUP BY Pessoa.name');
     $stmt->execute(array("%$search%", $_SESSION['local']));
     return $stmt->fetchall();
   }
@@ -33,12 +33,16 @@
     if(getsearchmodalidade($search)){
       $_SESSION["cat_mod"] = "modalidade";
       $_SESSION["info"] = $search;
-      return searchtreinador($search);
+      $res1 = searchtreinador($search);
+      $_SESSION["info_comp"] = $res1[0]['nome'];
+      return $res1;
     }
     else if(getsearchtreinador($search)){
       $_SESSION["cat_trein"] = "treinador";
       $_SESSION["info"] = $search;
-      return searchmodalidades($search);
+      $res2 = searchmodalidades($search);
+      $_SESSION["info_comp"] = $res2[0]['name'];
+      return $res2;
     }
     else {
       $_SESSION["msg"] = "termo não encontrado";
--- html/projeto/templates/searchresult.php
+++ html/projeto/templates/searchresult.php
@@ -1,20 +1,28 @@
 <?php
   session_start();
+
   $trein = $_SESSION["cat_mod"];
   unset($_SESSION["cat_mod"]);
+
   $mod = $_SESSION["cat_trein"];
   unset($_SESSION["cat_trein"]);
+
   $info = $_SESSION["info"];
   unset($_SESSION["info"]);
+
+  $info_comp = $_SESSION["info_comp"];
+  unset($_SESSION["info_comp"]);
 ?>
 
     <header>
-      <h1><a>Resultados<?php /*if(isset($_SESSION["cat_trein"])) $_SESSION["cat_trein"]
-                 if(isset($_SESSION["cat_mod"])) $_SESSION["cat_mod"]*/
-                 echo $mod;
-                 echo $trein;
-                 echo $info;
-       ?></a></h1>
+      <h1><a>Resultados Procura</a>
+        <a>"<?php echo $info; ?>"</a>
+        <a><?php echo $mod; ?></a>
+        <a><?php echo $trein; ?></a>
+        <a><?php echo $info_comp; ?></a>
+        <a>para a sua localidade</a>
+
+      </h1>
     </header>
 <section>
   <?php
