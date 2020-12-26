2dad5a04872df73fe45962459ecd8b1889e1947d
YsabellaRomero <75682047+YsabellaRomero@users.noreply.github.com>
2020-12-26 03:04:14 +0000

    lala

--- html/html/action_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_start();
-  require_once('config/init.php');
-
-  if(isset($_SESSION) && $_SESSION['loggedin'] == true) {
-    $_SESSION['loggedin'] = false;
-    header("Location: templates/homepage.php");
-  }
-?>
